c384b106f67c75f470274b9165de1427126d70ab
Jair Gomez <jagg1970@gmail.com>
2026-08-27 19:38:13 -0500

    V2 – Incorporación apartamento 201
    V2 – Incorporación apartamento 201

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C="lenc-v2-808-313-1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"]))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='lenc-v2-808-313-201-1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
