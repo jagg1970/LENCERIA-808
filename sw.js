71c4006c9ebe57abc1523fcdff4e55f90dea8ce5
Jair Gomez <jagg1970@gmail.com>
2026-08-27 20:55:37 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lenc-v2-808-313-201-2';
+const CACHE='lenc-v2-panel-lavanderia-2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
